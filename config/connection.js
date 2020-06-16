9c3a3caaf9bf735af4cd7440a4c229cc960e6c1a
Dan Fellner <dfellner92@gmail.com>
2020-06-16 18:08:50 -0400

    jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 const mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'burgers_db'
-});
-connection.connect((err) => {
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+}
+else {
+
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db'
+  });
+}
+  connection.connect((err) => {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
